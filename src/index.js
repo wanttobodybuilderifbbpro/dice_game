7cdd39e64ef822df1acaa936d8611a14509a489d
(wanttobodybuilderifbbpro) <(wogud114@gmail.com)>
2023-09-21 16:56:37 +0900

    second class

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const product = 'MacBook';
-const model = 'Air';
-const item = product + model;
-const imageUrl =
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/MacBook_with_Retina_Display.png/500px-MacBook_with_Retina_Display.png';
-
-function handelClick(e){
-  alert('Now It will be arriving!');
-}
-
-ReactDOM.render(
-  <>
-    <h1>{item} 주문하기</h1>
-    <img src={imageUrl} alt="제품 사진" />
-    <button onClick={handelClick}>확인</button>
-  </>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
