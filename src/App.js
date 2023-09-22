4d05bbf35fc6174e944d7b6f7328dff6802682b0
(wanttobodybuilderifbbpro) <(wogud114@gmail.com)>
2023-09-22 17:59:30 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import Dice from "./Dice";
 import Button from './Button';
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
 function App() {
+  const [num, setNum] = useState(1); // 첫 번째 : 현재 변수의 값 , 두 번째 : 파라미터로 전달하는 값으로 state가 변경됨
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+  }
+
+  const handleClearClick = () => {
+    setNum(1);
+  }
+
   return (
     <div>
       <div>
-        <Button>던지기</Button>
-        <Button>처음부터</Button>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="red" num={2} />
+      <Dice color="red" num={num} />
     </div>
   );
 }
